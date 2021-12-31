ea8614f400fa6c6b961f0d5cbe9f06874195a061
Gord Currie <gord@gordcurrie.com>
2021-12-31 01:39:17 -0600

    playing around

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # blockchain
+
+Working through https://blog.logrocket.com/how-to-build-blockchain-from-scratch-go/
---
+++ blockchain.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Block struct {
+	data         map[string]interface{}
+	hash         string
+	previousHash string
+	timestamp    time.Time
+	pow          int
+}
+
+type Blockchain struct {
+	genesisBlock Block
+	chain        []Block
+	difficulty   int
+}
+
+func (b Block) clacluateHash() string {
+	data, err := json.Marshal(b.data)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
+	blockhash := sha256.Sum256([]byte(blockData))
+
+	return fmt.Sprintf("%x", blockhash)
+}
+
+func (b *Block) mine(difficulty int) {
+	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
+		b.pow++
+		b.hash = b.clacluateHash()
+	}
+}
+
+func CreateBlockchain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		hash:      "0",
+		timestamp: time.Now(),
+	}
+
+	return Blockchain{
+		genesisBlock: genesisBlock,
+		chain:        []Block{genesisBlock},
+		difficulty:   difficulty,
+	}
+}
+
+func (b *Blockchain) addBlock(from, to string, amount float64) {
+	blockData := map[string]interface{}{
+		"from":   from,
+		"to":     to,
+		"amount": amount,
+	}
+
+	lastBlock := b.chain[len(b.chain)-1]
+	newBlock := Block{
+		data:         blockData,
+		previousHash: lastBlock.hash,
+		timestamp:    time.Now(),
+	}
+	newBlock.mine(b.difficulty)
+	b.chain = append(b.chain, newBlock)
+}
+
+func (b Blockchain) isValid() bool {
+	for i := range b.chain[1:] {
+		previousBlock := b.chain[i]
+		currentBlock := b.chain[i+1]
+		if currentBlock.hash != currentBlock.clacluateHash() || currentBlock.previousHash != previousBlock.hash {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	blockchain := CreateBlockchain(2)
+
+	blockchain.addBlock("Alice", "Bob", 5)
+	blockchain.addBlock("John", "Bob", 2)
+
+	fmt.Printf("%#v", blockchain)
+	fmt.Println(blockchain.isValid())
+}
